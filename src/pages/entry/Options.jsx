2ba1ffc4ed800d5acb9a998b62966f7242ba8363
Joe Cui <xiaozhou.cui@gmail.com>
2021-08-07 16:02:54 +1000

    Code and test scoop options images from server

--- src/pages/entry/Options.jsx
+++ src/pages/entry/Options.jsx
@@ -1,7 +1,33 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import ScoopOption from './ScoopOption';
+import { Row } from 'react-bootstrap';
 
 const Options = ({ optionType }) => {
-  return <div></div>;
+  const [items, setItems] = useState([]);
+
+  // optionType is 'scoops' or 'toppings'
+  useEffect(() => {
+    // GET req will be intercepted by Mock Service Worker in test env
+    axios
+      .get(`http://localhost:3030/${optionType}`)
+      .then((response) => setItems(response.data))
+      .catch((error) => {
+        // handle error response
+      });
+  }, [optionType]);
+
+  const ItemComponent = optionType === 'scoops' ? ScoopOption : null;
+
+  const optionItems = items.map((item) => (
+    <ItemComponent
+      key={item.name}
+      name={item.name}
+      imagePath={item.imagePath}
+    />
+  ));
+
+  return <Row>{optionItems}</Row>;
 };
 
 export default Options;
--- src/pages/entry/tests/Options.test.jsx
+++ src/pages/entry/tests/Options.test.jsx
@@ -2,10 +2,12 @@ import { render, screen } from '@testing-library/react';
 
 import Options from '../Options';
 
-test('displays image for each scoop option from server', () => {
+// http req is async, need to use "await" and "findBy" (cannot use "getBy")
+
+test('displays image for each scoop option from server', async () => {
   render(<Options optionType="scoops" />);
   // find all images by "alt" attribute, alt should end with "scoop"
-  const scoopImages = screen.getAllByRole('img', { name: /scoop$/i });
+  const scoopImages = await screen.findAllByRole('img', { name: /scoop$/i });
   // msw handler is returning 2 options: vanilla and chocolate
   expect(scoopImages).toHaveLength(2);
   // confirm alt text of images
