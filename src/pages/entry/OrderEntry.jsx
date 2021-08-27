44c6bbc4099aa4a85297f279dbc72782e8c67ff7
Joe Cui <xiaozhou.cui@gmail.com>
2021-08-27 21:48:27 +1000

    Disable order button if no scoops ordered

--- src/pages/entry/OrderEntry.jsx
+++ src/pages/entry/OrderEntry.jsx
@@ -4,13 +4,19 @@ import { useOrderDetails } from '../../contexts/OrderDetails';
 
 const OrderEntry = ({ setOrderPhase }) => {
   const [orderDetails] = useOrderDetails();
+
+  // disable order button if there aren't any scoops in order
+  const orderDisabled = orderDetails.totals.scoops === '$0.00';
+
   return (
     <div>
       <h1>Design Your Sundae!</h1>
       <Options optionType="scoops" />
       <Options optionType="toppings" />
       <h2>Grand total: {orderDetails.totals.grandTotal}</h2>
-      <Button onClick={() => setOrderPhase('review')}>Order Sundae!</Button>
+      <Button disabled={orderDisabled} onClick={() => setOrderPhase('review')}>
+        Order Sundae!
+      </Button>
     </div>
   );
 };
--- src/pages/entry/tests/OrderEntry.test.jsx
+++ src/pages/entry/tests/OrderEntry.test.jsx
@@ -6,6 +6,7 @@ import {
 import OrderEntry from '../OrderEntry';
 import { rest } from 'msw';
 import { server } from '../../../mocks/server';
+import userEvent from '@testing-library/user-event';
 
 test('handles error for scoops and toppings routes', async () => {
   // override the server handlers, to return 500 error
@@ -28,4 +29,25 @@ test('handles error for scoops and toppings routes', async () => {
     const alerts = await screen.findAllByRole('alert');
     expect(alerts).toHaveLength(2);
   });
+});
+
+test('disable order button if there are no scoops ordered', async () => {
+  render(<OrderEntry setOrderPhase={jest.fn()} />);
+
+  // order button should be disabled at first, even before options load
+  let orderButton = screen.getByRole('button', { name: /order sundae/i });
+  expect(orderButton).toBeDisabled();
+
+  // expect button to be enabled after adding scoop
+  const vanillaInput = await screen.findByRole('spinbutton', {
+    name: 'Vanilla',
+  });
+  userEvent.clear(vanillaInput);
+  userEvent.type(vanillaInput, '1');
+  expect(orderButton).toBeEnabled();
+
+  // expect button to be disabled after removing scoop
+  userEvent.clear(vanillaInput);
+  userEvent.type(vanillaInput, '0');
+  expect(orderButton).toBeDisabled();
 });
